affdda44f21d97796d03a3c757e572f28af04e84
Prabhav Khera <84613574+probro27@users.noreply.github.com>
2021-12-28 23:38:25 -0700

    Add files via upload

---
+++ addsum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> output(2);
+        for(int i = 0; i<nums.size()-1; i++){
+            for(int j = i+1; j<nums.size(); j++){
+                int sum = 0;
+                sum = nums[i] + nums[j];
+                if(sum == target){
+                    output[0] = i;
+                    output[1] = j;
+                    break;
+                }
+            }
+        }
+        return output;
+    }
