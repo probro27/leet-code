66c234c6cefad422aea4b1ade8698acfeb0e9bb0
Prabhav Khera <84613574+probro27@users.noreply.github.com>
2023-07-09 00:25:18 -0400

    Time: 57 ms (65.16%), Space: 41.9 MB (39.10%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: (newVal) => {
+            if (val === newVal) {
+                return true;
+            } else {
+                throw "Not Equal";
+            }
+        }, 
+        notToBe: (newVal) => {
+            if (val !== newVal) {
+                return true;
+            } else {
+                throw "Equal";
+            }
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
