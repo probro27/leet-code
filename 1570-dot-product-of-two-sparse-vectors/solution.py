f2f3a06d8103541ce0366fd129a4e7aa992f2591
probro27 <prabhavkhera@hotmail.com>
2024-10-29 20:38:03 -0400

    1570 dot product of two sparse vectors

---
+++ 1570-dot-product-of-two-sparse-vectors/solution.py
@@ -0,0 +1,17 @@
+class SparseVector:
+    def __init__(self, nums: List[int]):
+        self.nonZeroMap = {index: num for index, num in enumerate(nums) if num != 0}
+        
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        result = 0
+        nonZeroMap = {}
+        if len(self.nonZeroMap) <= len(vec.nonZeroMap):
+            nonZeroMap = self.nonZeroMap
+        else:
+            nonZeroMap = vec.nonZeroMap
+            
+        for index in nonZeroMap.keys():
+            if index in nonZeroMap:
+                result += self.nonZeroMap[index] * vec.nonZeroMap[index]
+        
+        return result
